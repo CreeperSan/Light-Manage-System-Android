1b63f04ae0890559e9d1ff892ff462b5c566a828
CreeperSan <sanc876@gmail.com>
2017-07-23 22:20:05 +0800

    .

---
+++ app/src/main/java/creepersan/smartlight/MainActivity.kt
@@ -0,0 +1,14 @@
+package creepersan.smartlight
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import creepersan.smartlight.base.BaseActivity
+
+class MainActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
